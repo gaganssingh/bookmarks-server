e93e0cc0cf6d2cc5a90ddd095299a7fc3668615a
Gagan Singh <gagansingh.dev@outlook.com>
2020-01-21 13:16:25 -0500

    prepading for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
-}
+	PORT              : process.env.PORT || 8000,
+	NODE_ENV          : process.env.NODE_ENV || "development",
+	API_TOKEN         : process.env.API_TOKEN || "dummy-api-token",
+	DATABASE_URL      : process.env.DB_URL || "postgresql://dunder-mifflin@localhost/bookmarks",
+	TEST_DATABASE_URL : process.env.DB_URL || "postgresql://dunder-mifflin@localhost/bookmarks-test"
+};
